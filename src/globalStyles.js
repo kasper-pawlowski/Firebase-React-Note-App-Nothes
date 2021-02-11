11dacd19c898da791fe1039898c58d40407bd2c5
Kasper Pawłowski <75247629+kasper-pawlowski@users.noreply.github.com>
2021-02-11 23:12:21 +0100

    Update globalStyles.js

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
     *, *::before, *::after {
         box-sizing: border-box;
@@ -15,11 +14,6 @@ const GlobalStyle = createGlobalStyle`
         overflow-x: hidden;
         overflow-y: hidden;
         background-color: #e4e7e7;
-    }
-    
-    textarea {
-        font-family: inherit;
-        font-size: inherit;
     }
 `;
  
